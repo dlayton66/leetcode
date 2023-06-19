919531e8b867f5623d1df073f5edd46ddd51ef48
Drew Layton <dlayton66@gmail.com>
2023-06-19 14:39:33 -0600

    feat: Best Time to Buy and Sell Stock - better solution

--- lib/best_time_to_buy_and_sell_stock.rb
+++ lib/best_time_to_buy_and_sell_stock.rb
@@ -1,20 +1,34 @@
+# def max_profit(prices)
+#   return 0 if prices.length <= 1
+#   max_profit = 0
+#   max_future_price, max_future_price_index = prices[1..].each_with_index.max
+
+#   prices[0..-2].each_with_index do |price1, i|
+#     next if price1 >= prices[i+1]
+    
+#     if i >= max_future_price_index
+#       price_index_pair = prices[i+1..].each_with_index.max
+#       max_future_price = price_index_pair[0]
+#       max_future_price_index = price_index_pair[1]+i+1
+#     end
+
+#     if max_future_price - price1 > max_profit
+#       max_profit = max_future_price - price1
+#     end
+#   end
+
+#   max_profit
+# end
+
 def max_profit(prices)
   return 0 if prices.length <= 1
   max_profit = 0
-  max_future_price, max_future_price_index = prices[1..].each_with_index.max
+  min_price = prices[0]
 
-  prices[0..-2].each_with_index do |price1, i|
-    next if price1 >= prices[i+1]
-    
-    if i >= max_future_price_index
-      price_index_pair = prices[i+1..].each_with_index.max
-      max_future_price = price_index_pair[0]
-      max_future_price_index = price_index_pair[1]+i+1
-    end
-
-    if max_future_price - price1 > max_profit
-      max_profit = max_future_price - price1
-    end
+  prices.each do |price|
+    min_price = price if price < min_price
+    profit = price - min_price
+    max_profit = profit if profit > max_profit
   end
 
   max_profit
